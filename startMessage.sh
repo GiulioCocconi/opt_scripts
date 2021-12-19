cb0dfca7472212293aa64d622a498b3dd52164d2
Giulio Cocconi <coccogiulio8@gmail.com>
2021-12-19 19:12:55 +0100

    Aggiunto uptime

--- startMessage.sh
+++ startMessage.sh
@@ -4,8 +4,13 @@ month=$(date +%m)
 year=$(date +%y)
 hour=$(date +%H)
 
+uptime=$(uptime -p)
+
 todo="$HOME/todo/$day.$month.$year/todo"
 msg="$HOME/todo/$day.$month.$year/msg"
+
+color_dir="/opt/shell-color-scripts"
+color_script="$color_dir/colorscript.sh"
 
 if [[ ! $# -eq 0 ]]; then
 	read -p "Per che giorno? [day.month.year] " data
@@ -20,7 +25,23 @@ if [[ ! $# -eq 0 ]]; then
 	exit
 fi
 
-echo "Oggi è il $day/$month/$year, sono le ore $hour"
+if [[ $SHOW_COLOR_SCRIPT != "" ]]; then
+	
+	if [[ ! -d $color_dir ]]; then
+		echo "Installo i color scripts..."
+		sudo git clone --depth=1 https://gitlab.com/dwt1/shell-color-scripts $color_dir > /dev/null
+		sudo chmod 755 $color_script
+		echo "color scripts installati :-)"
+	fi
+	
+	if [[ $SHOW_COLOR_SCRIPT == "RANDOM" ]]; then
+		bash $color_script -r
+	else
+		bash $color_script $SHOW_COLOR_SCRIPT
+	fi
+fi
+
+echo "Oggi è il $day/$month/$year, sono le ore $hour ($uptime)"
 
 if [[ -e $todo ]]; then
 	echo "Ci sono delle cose da fare per oggi..."
